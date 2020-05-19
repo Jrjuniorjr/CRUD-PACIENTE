5c22740b9db88c239b38b54f0985b42449907f16
Jrjuniorjr <mjuniorjrjr@gmail.com>
2020-05-19 20:18:35 -0300

    Teste

---
+++ src/main/java/com/example/demo/model/Paciente.java
@@ -0,0 +1,56 @@
+package com.example.demo.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_paciente")
+public class Paciente implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	private Integer id;
+	private String nome;
+	private String email;
+	
+	public Paciente() {
+		super();
+	}
+
+	public Paciente(Integer id, String nome, String email) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
